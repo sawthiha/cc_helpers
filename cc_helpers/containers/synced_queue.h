363a22adfa176c17fcced33f3f546a538465f9fe
sawthiha <sawthiha@mtu.edu.mm>
2022-04-01 01:02:56 +0900

    SyncedQueue moved push() is now an option!

--- cc_helpers/containers/synced_queue.h
+++ cc_helpers/containers/synced_queue.h
@@ -8,32 +8,38 @@ template <typename T>
 class SyncedQueue
 {
 private:
-   std::queue<T> m_queue;
-   absl::Mutex m_mutex;
+    std::queue<T> m_queue;
+    absl::Mutex m_mutex;
 
-   bool not_empty() const
-   {
-       return !m_queue.empty();
-   }
+    bool not_empty() const
+    {
+        return !m_queue.empty();
+    }
 
 public:
-   void push(const T& item)
-   {
-       absl::WriterMutexLock writer_lock(&m_mutex);
-       m_queue.push(std::move(item));
-   }
+    void push(const T& item)
+    {
+        absl::WriterMutexLock writer_lock(&m_mutex);
+        m_queue.push(item);
+    }
+   
+    void push(T&& item)
+    {
+        absl::WriterMutexLock writer_lock(&m_mutex);
+        m_queue.push(std::move(item));
+    }
 
-   std::optional<T> pop()
-   {
-       if(m_mutex.LockWhenWithTimeout(absl::Condition(this, &SyncedQueue<T>::not_empty), absl::Milliseconds(400)))
-       {
-           auto result = std::move(m_queue.front());
-           m_queue.pop();
-           m_mutex.Unlock();
-           return result;
-       }
-       return std::nullopt;
-   }
+    std::optional<T> pop()
+    {
+        if(m_mutex.LockWhenWithTimeout(absl::Condition(this, &SyncedQueue<T>::not_empty), absl::Milliseconds(400)))
+        {
+            T result = std::move(m_queue.front());
+            m_queue.pop();
+            m_mutex.Unlock();
+            return result;
+        }
+        return std::nullopt;
+    }
 
 };
 
@@ -51,6 +57,13 @@ public:
         absl::WriterMutexLock lock(&m_queue_mutex);
         m_queue_map.try_emplace(stream_name);
         m_queue_map[stream_name].push(output);
+    }
+
+    void push(const std::string& stream_name, T&& output)
+    {
+        absl::WriterMutexLock lock(&m_queue_mutex);
+        m_queue_map.try_emplace(stream_name);
+        m_queue_map[stream_name].push(std::move(output));
     }
 
     std::optional<T> pop(const std::string& stream_name)
